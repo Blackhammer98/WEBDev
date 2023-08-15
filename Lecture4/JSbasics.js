3c62d878d55a0377cd7266f29bea4aff28b351a6
nikit <snikit086@gmail.com>
2023-08-15 13:57:25 +0530

    Lecture4

---
+++ Lecture4/JSbasics.js
@@ -0,0 +1,12 @@
+var a= 10;
+document.write("55",typeof a);
+a = "string";
+document.write("57" ,typeof a);
+
+
+
+/*/ non primitive datatypes/*/
+const person ={
+    name :"nikit",
+    age:26
+}
---
+++ Lecture4/codeexecution.js
@@ -0,0 +1,24 @@
+//document.write(x);
+//ar x="nikit";
+
+// sayHi(); 
+ // function sayHi(){
+   // console.log("hello")
+  //}
+
+/*/var  fruits = "apple";
+console.log(fruits);
+
+{
+    
+   let fruits;
+    console.log(fruits);
+      fruits = "oraanges";
+      {
+        console.log(fruits);
+
+      }
+      console.log(fruits);
+      git
+}
+console.log(fruits);/*/
